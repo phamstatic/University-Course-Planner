4820e1094943568042dce88c884a6ef1a219ac03
John Pham <phambinouh@gmail.com>
2024-01-19 22:30:24 -0600

    LocalStorage added

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+localStorage.setItem("toggleDark", "false");
+
+let tableData = document.querySelectorAll('td');
+const toDark = function() {
+    for (let i = 0; i < tableData.length; i++) {
+        tableData[i].style.backgroundColor = "black";
+    }
+}
+
+let toggleDark = document.querySelector("#toggleEmoji");
+toggleDark.addEventListener('click', function() {
+    if (localStorage.getItem("toggleDark") === "false") {
+        localStorage.setItem("toggleDark", "true")
+        toggleDark.innerHTML = "🌙";
+        toDark();
+    }
+    else {
+        localStorage.setItem("toggleDark", "false")
+        toggleDark.innerHTML = "☀️";
+    }
+})
+
 let courseList = document.querySelectorAll('tbody tr');
 for (let i = 0; i < courseList.length; i++) {
     courseList[i].addEventListener('click', function() {
