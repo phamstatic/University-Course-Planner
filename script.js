09639678d2432d8d3665dbdd42b353ec82aa62b2
John Pham <phambinouh@gmail.com>
2024-01-20 00:51:29 -0600

    Course container + transition animation

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 localStorage.setItem("toggleDark", "false");
 
-let tableData = document.querySelectorAll('td');
-const toDark = function() {
-    for (let i = 0; i < tableData.length; i++) {
-        tableData[i].style.backgroundColor = "black";
-    }
-}
-
 let toggleDark = document.querySelector("#toggleEmoji");
-toggleDark.addEventListener('click', function() {
+toggleDark.addEventListener("click", function() {
     if (localStorage.getItem("toggleDark") === "false") {
         localStorage.setItem("toggleDark", "true")
         toggleDark.innerHTML = "🌙";
@@ -20,10 +13,24 @@ toggleDark.addEventListener('click', function() {
     }
 })
 
-let courseList = document.querySelectorAll('tbody tr');
+let tableData = document.querySelectorAll("td");
+const toDark = function() {
+    for (let i = 0; i < tableData.length; i++) {
+        tableData[i].style.backgroundColor = "black";
+        tableData[i].style.color = "white";
+    }
+}
+
+let courseList = document.querySelectorAll("tbody tr");
 for (let i = 0; i < courseList.length; i++) {
     courseList[i].addEventListener('click', function() {
         let courseNumber = this.querySelector('td:first-child').innerHTML;
         console.log(`User clicked ${courseNumber}`);
     })
 }
+
+let courseClose = document.querySelector("#courseClose");
+let courseContainer = document.querySelector("#courseContainer");
+courseClose.addEventListener("click", function() {
+    courseContainer.classList.toggle("fade");
+})
