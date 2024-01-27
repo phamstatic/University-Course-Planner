0508088d3ca4d4cbe77c4b0f7198276d09899198
John Pham <phambinouh@gmail.com>
2024-01-26 22:57:07 -0600

    Recoding the JavaScript file

---
+++ script.js
@@ -0,0 +1,21 @@
+$(function() {
+    // First Time Load
+    if (localStorage.getItem("degreesList") === null) {
+        fetch("degreesList.json")
+        .then(res => res.json())
+        .then(jsonData => {
+            localStorage.setItem("degreesList", JSON.stringify(jsonData));
+            console.log("degreesList.json was loaded for the first time.");
+            console.log(jsonData);
+        });
+    }
+    if (localStorage.getItem("classList") === null) {
+        fetch("classList.json")
+        .then(res => res.json())
+        .then(jsonData => {
+            localStorage.setItem("classList", JSON.stringify(jsonData));
+            console.log("classList.json was loaded for the first time.");
+            console.log(jsonData);
+        });
+    }
+})
